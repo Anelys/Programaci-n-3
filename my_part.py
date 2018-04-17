4f53926c228c332fa249eb62329495e10506b9c6
Anelys <32472435+Anelys@users.noreply.github.com>
2018-04-16 19:21:39 -0500

    Add files via upload

---
+++ my_part.py
@@ -0,0 +1,58 @@
+import re
+import os
+
+def crearArchivoProductos():
+    arch = open('productos.txt', 'w')
+    #arch.close()
+
+def descriptions():
+    arch = open('productos.txt','a')
+    description = input("Ingrese Nombre de productos:")
+    arch.write(description)
+    arch.write("\n")
+
+def codes():
+    arch = open('productos.txt','a')
+    code = input("Ingrese codigo del producto:")
+    arch.write(code)
+    arch.write("\n")
+"""a+=1
+    if a > 1:
+        arch = open('productos.txt','r')
+        lines = arch.readlines()
+        for line in lines:
+            if word in line:
+                print (line)"""
+
+def prices():
+    arch = open('productos.txt','a')
+    price = input("Ingrese precio del producto: ")
+    arch.write(price)
+    arch.write("\n")
+
+def itbms():
+    arch = open('productos.txt','a')
+    itbm = input("Ingrese impuesto del producto: ")
+    arch.write(itbm)
+    arch.write("\n")
+
+def units():
+    arch = open('productos.txt','a')
+    unit = input("Ingrese unidad del producto L o G: ")
+    arch.write(unit)
+    arch.write("\n")
+ 
+if __name__ == "__main__":
+    print ("Ingrese cantidad de productos ")
+    resp = int(input())
+    crearArchivoProductos()
+    i = 0
+    os.system("cls")
+    while i != resp:
+        descriptions()
+        codes()
+        prices()
+        itbms()
+        units()
+        i += 1
+        os.system("cls")
