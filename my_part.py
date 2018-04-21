7e76e4d9d697e2c7a497f8f5e29d4e582a84fe0f
Anelys <anelys.0497@gmail.com>
2018-04-20 20:43:52 -0500

    Intento de validacion

--- my_part.py
+++ my_part.py
@@ -1,76 +1,78 @@
 import re
 import os
 
-def crearArchivoProductos():
-    arch = open('productos.txt', 'w')
-    #arch.close()
+class usuariosCodigo (object):
+    def crearArchivoProductos(self):
+        f = open('productos.txt', 'w')
+        # arch.close()
 
-def descriptions():
-    arch = open('productos.txt','a')
-    description = input("Ingrese Nombre de productos:")
-    arch.write(description)
-    arch.write("\n")
+    def validateCode(self, validateCodes):
+        try:
+            f = open('productos.txt', 'r')
+        except FileNotFoundError:
+            print("--> ERROR!! No Existe Archivo de Usuarios")
+            return (False)
 
-def codes():
-    arch = open('productos.txt','a')
-    code = input("Ingrese codigo del producto:")
-    arch.write(code)
-    arch.write("\n")
-    if (validateCode(value) == True)
-      text = (input("Este codigo ya existe"))
-"""a+=1
-    if a > 1:
-        arch = open('productos.txt','r')
-        lines = arch.readlines()
-        for line in lines:
-            if word in line:
-                print (line)"""
+        for linea in f.readlines():
 
-def prices():
-    arch = open('productos.txt','a')
-    price = input("Ingrese precio del producto: ")
-    arch.write(price)
-    arch.write("\n")
+            linea = linea.split(',')
+            description = linea [0]
+            code = linea[1]
+            price = linea [2]
+            itbm = linea [3]
+            unit = linea [4]
 
-def itbms():
-    arch = open('productos.txt','a')
-    itbm = input("Ingrese impuesto del producto: ")
-    arch.write(itbm)
-    arch.write("\n")
+            if (validateCodes == code):
+                return (True)
+            return (False)
 
-def units():
-    arch = open('productos.txt','a')
-    unit = input("Ingrese unidad del producto L o G: ")
-    arch.write(unit)
-    arch.write("\n")
-    
-def validateCode(self, validateCodes):
-    try:
-        f = open('productos.txt','r')
-    except FileNotFoundError:
-        print ("--> ERROR!! No Existe Archivo de Usuarios")  
-        return (False) 
+    def descriptions(self):
+        f = open('productos.txt', 'a')
+        description = input("Ingrese Nombre de productos:")
+        f.write(description)
+        f.write("\n")
 
-    for linea in f.readlines():
+    def codes(self):
+        f = open('productos.txt', 'a')
+        code = input("Ingrese codigo del producto:")
+        f.write(code)
+        f.write("\n")
+        validate = usuariosCodigo()
+        if (validate.validateCode(code) == True):
+            print ("Este codigo ya existe <ENTER>")
 
-        linea = linea.split(',')
-        code = linea[1]
 
-        if (validateCodes == code):
-            return (True)
-    return (False)
- 
-if __name__ == "__main__":
-    print ("Ingrese cantidad de productos ")
-    resp = int(input())
-    crearArchivoProductos()
-    i = 0
+    def prices(self):
+        f = open('productos.txt', 'a')
+        price = input("Ingrese precio del producto: ")
+        f.write(price)
+        f.write("\n")
+
+
+    def itbms(self):
+        f = open('productos.txt', 'a')
+        itbm = input("Ingrese impuesto del producto: ")
+        f.write(itbm)
+        f.write("\n")
+
+
+    def units(self):
+        f = open('productos.txt', 'a')
+        unit = input("Ingrese unidad del producto L o G: ")
+        f.write(unit)
+        f.write("\n")   
+
+x = usuariosCodigo()
+print("Ingrese cantidad de productos ")
+resp = int(input())
+x.crearArchivoProductos()
+i = 0
+os.system("cls")
+while i != resp:
+    x.descriptions()
+    x.codes()
+    x.prices()
+    x.itbms()
+    x.units
+    i += 1
     os.system("cls")
-    while i != resp:
-        descriptions()
-        codes()
-        prices()
-        itbms()
-        units()
-        i += 1
-        os.system("cls")
